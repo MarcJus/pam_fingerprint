c98533628db56d64ed474e7121dd5ca3a9b47865
MarcJus <marcjuss@proton.me>
2023-07-31 21:59:28 +0200

    Init pam module
    Get tty name for priting messages.

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <security/pam_modules.h>
+#include <security/pam_misc.h>
+#include <security/pam_ext.h>
+#include <syslog.h>
+
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const char **argv ){
+	ssize_t ret;
+	int tty_fd;
+	const char *tty_name;
+
+	ret = pam_get_item(pamh, PAM_TTY, (const void **)&tty_name);
+
+	if(tty_name == NULL){
+		tty_name = ttyname(STDOUT_FILENO);
+		if(tty_name == NULL)
+			pam_syslog(pamh, LOG_ALERT, "Could not get tty name");
+	}
+
+	pam_syslog(pamh, LOG_DEBUG, "tty: %s", tty_name);
+
+	return PAM_ABORT;
+}
+
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv){
+	return PAM_SUCCESS;
+}
