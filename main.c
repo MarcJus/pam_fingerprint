99fb76c7b89ecbc39e95cac1afd7e52d54fcfe90
MarcJus <marcjuss@proton.me>
2023-07-31 22:08:45 +0200

    Open tty file for printing messages
    The module will fail if it could not open the tty file.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <security/pam_misc.h>
 #include <security/pam_ext.h>
 #include <syslog.h>
+#include <errno.h>
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const char **argv ){
 	ssize_t ret;
@@ -15,11 +16,19 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const
 
 	if(tty_name == NULL){
 		tty_name = ttyname(STDOUT_FILENO);
-		if(tty_name == NULL)
+		if(tty_name == NULL){
 			pam_syslog(pamh, LOG_ALERT, "Could not get tty name");
+			tty_name = "/dev/tty";
+		}
 	}
 
 	pam_syslog(pamh, LOG_DEBUG, "tty: %s", tty_name);
+
+	tty_fd = open(tty_name, O_RDWR);
+	if(tty_fd < 0){
+		pam_syslog(pamh, LOG_ERR, "Could not open tty: %s", strerror(errno));
+		return PAM_ABORT;
+	}
 
 	return PAM_ABORT;
 }
