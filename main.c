544c1761562745c73527a78600bee1c4b4d11fbd
MarcJus <marcjuss@proton.me>
2023-08-08 12:22:37 +0200

    Delete tty
    We are gonna use pam_prompt()

--- main.c
+++ main.c
@@ -9,43 +9,8 @@
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const char **argv ){
 	ssize_t ret;
-	int tty_fd;
 	int fingerprint_fd;
-	const char *tty_name;
 	uint8_t *fingerprint_buffer;
-
-	ret = pam_get_item(pamh, PAM_TTY, (const void **)&tty_name);
-
-	if(tty_name == NULL){
-		tty_name = ttyname(STDOUT_FILENO);
-		if(tty_name == NULL){
-			pam_syslog(pamh, LOG_ALERT, "Could not get tty name");
-			tty_name = "/dev/tty";
-		}
-	}
-
-	pam_syslog(pamh, LOG_DEBUG, "tty: %s", tty_name);
-
-	tty_fd = open(tty_name, O_RDWR);
-	if(tty_fd < 0){
-		pam_syslog(pamh, LOG_ERR, "Could not open tty: %s", strerror(errno));
-		ret = PAM_ABORT;
-		goto exit;
-	}
-
-	ret = dup2(tty_fd, STDOUT_FILENO);
-	if(tty_fd < 0){
-		pam_syslog(pamh, LOG_ERR, "Error duplicating stdout: %s", strerror(errno));
-		ret = PAM_ABORT;
-		goto exit;
-	}
-
-	ret = dup2(tty_fd, STDERR_FILENO);
-	if(tty_fd < 0){
-		pam_syslog(pamh, LOG_ERR, "Error duplicating stderr: %s", strerror(errno));
-		ret = PAM_ABORT;
-		goto exit;
-	}
 
 	fingerprint_fd = open("/dev/fingerprint", O_RDONLY);
 	if(fingerprint_fd < 0){
